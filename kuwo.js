2998a2bcca8a30085ef554b65e266b866a76d08a
胖哥 <57278320+mr-pangpang@users.noreply.github.com>
2021-10-01 11:35:08 +0800

    Update kuwo.js

--- kuwo.js
+++ kuwo.js
@@ -5,7 +5,7 @@
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://gitee.com/mr-pangpang/script/raw/master/kuwo.js
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/mr-pangpang/script/main/kuwo.js
 [mitm]
 hostname = vip1.kuwo.cn
 
@@ -13,8 +13,7 @@ hostname = vip1.kuwo.cn
 Surge4 or Loon:
 
 [Script]
-http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://gitee.com/mr-pangpang/script/raw/master/kuwo.js
-
+http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/mr-pangpang/script/main/kuwo.js
 [MITM]
 hostname = vip1.kuwo.cn
 
